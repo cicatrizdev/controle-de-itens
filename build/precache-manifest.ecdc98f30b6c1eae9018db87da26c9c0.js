7b9426e29235b84eefe4fbb4eff36cde0ce1345c
cicatrizwp <pedrocmello.bsi@gmail.com>
2019-03-18 20:23:44 -0300

    .

---
+++ build/precache-manifest.ecdc98f30b6c1eae9018db87da26c9c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "695e539b934a688c74df",
+    "url": "/controle-de-itens/static/js/runtime~main.6a0f698d.js"
+  },
+  {
+    "revision": "a2c457ace5da945ef943",
+    "url": "/controle-de-itens/static/js/main.9c4480b7.chunk.js"
+  },
+  {
+    "revision": "50db364589e498a2aede",
+    "url": "/controle-de-itens/static/js/2.011a8211.chunk.js"
+  },
+  {
+    "revision": "a2c457ace5da945ef943",
+    "url": "/controle-de-itens/static/css/main.3cfff78a.chunk.css"
+  },
+  {
+    "revision": "a9848c316aa3301712e50f2813564c33",
+    "url": "/controle-de-itens/index.html"
+  }
+];
